1daa3e470286489af0334c452dc133504c043192
redha <ahred.mbrk@gmail.com>
2024-07-03 17:26:08 +0100

    Added bear package to generate LSP using ccls

--- shell.nix
+++ shell.nix
@@ -6,11 +6,11 @@ pkgs.mkShell
     cmake
     gcc-arm-embedded
     python3
-
+    bear
   ];
 
   shellHook = ''
     echo "Raspberry Pi Pico Development Env"
-    export PICO_SDK_PATH=/home/redha/wrk/repo/pi-pico/sdk/
+    export PICO_SDK_PATH=/home/redha/wrk/repo/pi-pico/pico-sdk/
   '';
 }
